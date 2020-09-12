a951f21afe9c3f6fe9a2155b3b1b65f0ee6545d8
Evgeny Shpilevsky <eshpilevsky@icloud.com>
2020-09-12 19:01:58 +0300

    Added ssl

--- server/config/config.js
+++ server/config/config.js
@@ -96,9 +96,9 @@ module.exports =
 	// if you use encrypted private key the set the passphrase
 	tls:
 	{
-		cert: `${__dirname}/../certs/cert.pem`,
+		cert: `/etc/letsencrypt/live/newedutech.xyz/fullchain.pem`,
 		// passphrase: 'key_password'
-		key: `${__dirname}/../certs/privkey.pem`
+		key: `/etc/letsencrypt/live/newedutech.xyz/privkey.pem`
 	},
 	// listening Host or IP
 	// If omitted listens on every IP. ("0.0.0.0" and "::")
